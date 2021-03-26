2f7d8a4c5b9b8abfa304ecc41fa20d7b1f8121dc
mbrzus <michal-brzus@uiowa.edu>
2021-03-25 20:59:50 -0500

    update data part of gan lightning module

--- code/GAN/GAN.py
+++ code/GAN/GAN.py
@@ -10,7 +10,6 @@ import matplotlib.pyplot as plt
 import pytorch_lightning as pl
 import torch
 import json
-import itk
 import apex
 import numpy as np
 
@@ -25,19 +24,20 @@ from monai.networks.nets import UNet
 from monai.transforms import (
     AddChanneld,
     Compose,
-    CropForegroundd,
-    LoadNiftid,
+    LoadImaged,
     Orientationd,
-    RandCropByPosNegLabeld,
+    SpatialPadd,
     ScaleIntensityRanged,
+    ResizeWithPadOrCropd,
+    RandRotated,
+    RandGaussianNoised,
     Spacingd,
     ToTensord,
-    ThresholdIntensityd
 )
 from monai.utils import set_determinism
 
 
-class BrainExtraction(pl.LightningModule):
+class CrossModGan(pl.LightningModule):
     def __init__(self, lr=0.001):
         super().__init__()
         self.model = UNet(
@@ -81,108 +81,50 @@ class BrainExtraction(pl.LightningModule):
         return val_loss
 
     def prepare_data(self):
-        with open('../metadata/label_paths.json', 'r') as openfile:
-            label_json = json.load(openfile)
-        with open('../metadata/image_paths.json', 'r') as openfile:
-            image_json = json.load(openfile)
+        with open('../metadata/T1w_paths.json', 'r') as openfile:
+            t1w_json = json.load(openfile)
+        with open('../metadata/T2w_paths.json', 'r') as openfile:
+            t2w_json = json.load(openfile)
 
-        train_labels = label_json["train"]
-        train_images = image_json["train"]
-        val_labels = label_json["val"]
-        val_images = image_json["val"]
-        test_labels = label_json["test"]
-        test_images = image_json["test"]
+        train_labels = t1w_json["train"]
+        train_images = t2w_json["train"]
+        val_labels = t1w_json["val"]
+        val_images = t2w_json["val"]
+        test_labels = t1w_json["test"]
+        test_images = t2w_json["test"]
 
         train_files = [
-            {"image": image_name, "label": label_name}
-            for image_name, label_name in zip(train_images, train_labels)
+            {"T1w": t1w_path, "T2w": t2w_path}
+            for t1w_path, t2w_path in zip(train_images, train_labels)
         ]
         val_files = [
-            {"image": image_name, "label": label_name}
-            for image_name, label_name in zip(val_images, val_labels)
+            {"T1w": t1w_path, "T2w": t2w_path}
+            for t1w_path, t2w_path in zip(val_images, val_labels)
         ]
         test_files = [
-            {"image": image_name, "label": label_name}
-            for image_name, label_name in zip(test_images, test_labels)
+            {"T1w": t1w_path, "T2w": t2w_path}
+            for t1w_path, t2w_path in zip(test_images, test_labels)
         ]
 
         train_files = train_files[:150]
         val_files = val_files[:15]
         test_files = test_files[:15]
 
-        train_transforms = Compose(
+        transforms = Compose(
             [
-                LoadNiftid(keys=["image", "label"]),
-                AddChanneld(keys=["image", "label"]),
-                Spacingd(keys=["image", "label"], pixdim=(1, 1, 1), mode=("bilinear", "nearest")),
-                Orientationd(keys=["image", "label"], axcodes="RAS"),
-                ThresholdIntensityd(keys=["label"], threshold=1, above=False, cval=1),
-                CropForegroundd(keys=["image", "label"], source_key="image"),
-                # randomly crop out patch samples from big image based on pos / neg ratio
-                # the image centers of negative samples must be in valid image area
-                RandCropByPosNegLabeld(
-                    keys=["image", "label"],
-                    label_key="label",
-                    spatial_size=(96, 96, 96),
-                    pos=1,
-                    neg=1,
-                    num_samples=4,
-                    image_key="image",
-                    image_threshold=0,
-                ),
+                LoadImaged(keys=["t1w", "t2w"]),
+                AddChanneld(keys=["t1w", "t2w"]),
+                #Orientationd(keys=["t1w", "t2w"], axcodes="RAS"),
+                ResizeWithPadOrCropd(keys=["t1w", "t2w"], spatial_size=[288, 288, 288]),
                 ToTensord(keys=["image", "label"]),
+            ]
+        )
 
-            ]
-        )
-        val_transforms = Compose(
-            [
-                LoadNiftid(keys=["image", "label"]),
-                AddChanneld(keys=["image", "label"]),
-                Spacingd(
-                    keys=["image", "label"], pixdim=(1, 1, 1), mode=("bilinear", "nearest"),
-                ),
-                Orientationd(keys=["image", "label"], axcodes="RAS"),
-                ThresholdIntensityd(
-                    keys=["label"],
-                    threshold=1,
-                    above=False,
-                    cval=1
-                ),
-                CropForegroundd(keys=["image", "label"], source_key="image"),
-                ToTensord(keys=["image", "label"]),
-            ]
-        )
-        test_transforms = Compose(
-            [
-                LoadNiftid(keys=["image", "label"]),
-                AddChanneld(keys=["image", "label"]),
-                Spacingd(
-                    keys=["image", "label"], pixdim=(1, 1, 1), mode=("bilinear", "nearest"),
-                ),
-                Orientationd(keys=["image", "label"], axcodes="RAS"),
-                ThresholdIntensityd(
-                    keys=["label"],
-                    threshold=1,
-                    above=False,
-                    cval=1
-                ),
-                CropForegroundd(keys=["image", "label"], source_key="image"),
-                ToTensord(keys=["image", "label"]),
-            ]
-        )
 
         # we use cached datasets - these are 10x faster than regular datasets
-        self.train_dataset = CacheDataset(data=train_files, transform=train_transforms, cache_rate=1.0, num_workers=4,)
-        #print(self.train_ds)
-        #for i in range(5):
-        #    print(np.shape(self.train_ds[i]))
-        # for i in range(0,1000):
-        #     for j in range(0,10):
-        #         current = self.train_ds[i]
-        #         #ve_to_disc(f"temp image_{i}_rand{j}.png", current) #for the first 1000 images in the cache, get 10 samples from the image
-        #         #write a function save to disc that save a html report containing the center slices of the 96x96x96 image
-        self.val_dataset = CacheDataset(data=val_files, transform=val_transforms, cache_rate=1.0, num_workers=4,)
-        self.test_dataset = CacheDataset(data=test_files, transform=test_transforms, cache_rate=1.0, num_workers=4, )
+        self.train_dataset = CacheDataset(data=train_files, transform=transforms, cache_rate=1.0, num_workers=4,)
+        self.val_dataset = CacheDataset(data=val_files, transform=transforms, cache_rate=1.0, num_workers=4,)
+        self.test_dataset = CacheDataset(data=test_files, transform=transforms, cache_rate=1.0, num_workers=4, )
 
     def train_dataloader(self):
         train_loader = torch.utils.data.DataLoader(self.train_dataset, batch_size=5, shuffle=True, num_workers=4)
@@ -199,37 +141,40 @@ class BrainExtraction(pl.LightningModule):
 
 if __name__ == "__main__":
     print_config()
-    root_dir = "/home/mbrzus/programming/masterthesis/code"
+    root_dir = "/home/mbrzus/programming/Cross-Modality-Minipig-Gan/code"
 
     # set up loggers and checkpoints
-    log_dir = os.path.join(root_dir, "CNN/logs")
-    tb_logger = pl.loggers.TensorBoardLogger(
-        save_dir=log_dir
-    )
+    log_dir = os.path.join(root_dir, "GAN/logs")
+    tb_logger = pl.loggers.TensorBoardLogger(save_dir=log_dir)
     checkpoint_callback = pl.callbacks.model_checkpoint.ModelCheckpoint(
-        filepath=os.path.join(log_dir, "{epoch}-{val_loss:.2f}-{val_dice:.2f}")
+        dirpath=log_dir,
+        filename="{epoch}-{val_loss:.2f}-{val_dice:.2f}",
+        save_top_k=1,
+        verbose=True,
+        monitor='val_loss',
+        mode='min'
     )
 
     # initialise Lightning's trainer.
     trainer = pl.Trainer(
         gpus=[0],
-        max_epochs=1,
+        max_epochs=5,
         logger=tb_logger,
         checkpoint_callback=checkpoint_callback,
         # precision=16
-        amp_backend='apex',
-        amp_level='O3'
-        #num_sanity_val_steps=1,
-        #auto_lr_find=False
+        # amp_backend='apex',
+        # amp_level='O3'
+        # num_sanity_val_steps=1,
+        # auto_lr_find=False
     )
 
-    model = BrainExtraction()
-    #trainer.tune(model)
+    model = CrossModGan()
+    # trainer.tune(model)
     import time
+
     start_time = time.time()
     trainer.fit(model)
     print("--- %s seconds ---" % (time.time() - start_time))
-
 
     model.eval()
     device = torch.device("cuda:0")
@@ -241,19 +186,12 @@ if __name__ == "__main__":
             test_outputs = sliding_window_inference(
                 test_data["image"].to(device), roi_size, sw_batch_size, model
             )
-
-            # Inference part with saving image to disk
-            if i == 0:
-                path_to_first_input_test = test_data.__getitem__(0)
-            np_image = torch.argmax(test_outputs, dim=1).detach().cpu().numpy()[0]
-            np_image = np_image.astype('int16')  # change the number data type
-            nifti_image = itk.image_from_array(np_image) # look deeper into how to change this with the use ITK and make sure the image is properly configuret
-            # make sure it is in the same physical space as the input - working on it in the itk_inference_postprocessing file
-            #output_name = f"inferred_{i}_{os.path.basename(model.train_dataset.data[i]['label'])}"
-            #itk.imwrite(nifti_image, f"inferred_test_images/{output_name}")
-
-
             # plot the slice [:, :, 80]
+
+            np_image = torch.argmax(test_outputs, dim=1).detach().cpu().numpy()
+            meta = test_data['image_meta_dict']
+            print(meta)
+
             plt.figure("check", (18, 6))
             plt.subplot(1, 3, 1)
             plt.title(f"image {i}")
@@ -263,6 +201,6 @@ if __name__ == "__main__":
             plt.imshow(test_data["label"][0, 0, :, :, 80])
             plt.subplot(1, 3, 3)
             plt.title(f"output {i}")
-            plt.imshow(torch.argmax(test_outputs, dim=1).detach().cpu()[0, :, :, 80]) # analyze what number should be here
+            plt.imshow(torch.argmax(test_outputs, dim=1).detach().cpu()[0, :, :, 80])
             plt.show()
-    print(path_to_first_input_test)
+
