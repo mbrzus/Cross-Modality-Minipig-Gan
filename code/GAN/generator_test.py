863c6d88a65e2300e3bf5c3210559b9b6ac55ab3
Chase Johnson <cjohnson30@uiowa.edu>
2021-04-14 16:13:12 -0500

    Reverted the channel sizes back to that originally used to decrease the complexity of the generator.

--- code/GAN/generator_test.py
+++ code/GAN/generator_test.py
@@ -44,10 +44,11 @@ from monai.utils import set_determinism
 from torch.utils.data import DataLoader, random_split
 from monai.visualize.img2tensorboard import plot_2d_or_3d_image
 
+
 class CasNetGenerator(nn.Module):
     # source: https://arxiv.org/pdf/1806.06397.pdf
     def __init__(
-        self, img_shape, n_unet_blocks=6 # The MEDGAN paper had the best results with 6 unet blocks
+        self, img_shape, n_unet_blocks=6
     ):  # TODO: change num u_net blocks for actual trraining
         super().__init__()
         self.img_shape = img_shape
@@ -55,8 +56,8 @@ class CasNetGenerator(nn.Module):
         def unet_block(
             in_channels,
             out_channels,
-            channels=(64, 128, 256, 512, 512, 512, 512),#, 512),
-            strides=(2, 2, 2, 2, 2, 2, 2),#, 2),
+            channels=(16, 32, 64, 128),
+            strides=(2, 2, 2),
         ):
             return UNet(
                 dimensions=3,
@@ -75,6 +76,7 @@ class CasNetGenerator(nn.Module):
 
     def forward(self, x):
         return self.model(x)
+
 
 if __name__ == "__main__":
     arr = np.ones((128, 128, 128))
