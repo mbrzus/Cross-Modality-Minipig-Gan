d31a66b226bb19e23e8628dc67edd2949b7db79b
Chase Johnson <cjohnson30@uiowa.edu>
2021-04-23 16:17:34 -0500

    Renamed file variables to clarify what they represent.

--- code/GAN/cnn_inferrence.py
+++ code/GAN/cnn_inferrence.py
@@ -66,11 +66,11 @@ class BrainExtraction(pl.LightningModule):
 
 if __name__ == "__main__":
 
-    checkpoints_dir = "/Shared/sinapse/aml/incorrect-resampler/casnet-gen_patchgan-disc"
+    checkpoints_dir = "/Shared/sinapse/aml/correct-resampler/casnet-gen_patchgan-disc"
 
     # define model and load its parameters
     model = BrainExtraction.load_from_checkpoint(
-        checkpoint_path=f"{checkpoints_dir}/gen_epoch=0-g_loss=100.08-g_recon_loss=0.08-d_loss=45.00.ckpt",
+        checkpoint_path=f"{checkpoints_dir}/gen_epoch=20-g_loss=100.00-g_recon_loss=0.00-d_loss=45.00.ckpt",
         hparams_file=f"{checkpoints_dir}/default/version_0/hparams.yaml",
         strict=False
     )
@@ -90,47 +90,27 @@ if __name__ == "__main__":
         t2_json = json.load(openfile)
 
 
-    test_labels = t1_json["test"]
-    test_images = t2_json["test"]
+    test_T1s = t1_json["test"]
+    test_T2s = t2_json["test"]
 
     # zip the test data into a dictionary form
     test_files = [
-        {"image": image_name, "label": label_name}
-        for image_name, label_name in zip(test_images, test_labels)
+        {"T1": t1, "T2": t2}
+        for t2, t1 in zip(test_T2s, test_T1s)
     ]
-    test_files = test_files[:3] # for testing use just 1 image
+    test_files = test_files[:1] # for testing use just 1 image
 
     # define transforms for the data
-    # transforms = Compose(
-    #     [
-    #         LoadImaged(keys=["image", "label"]),
-    #         AddChanneld(keys=["image", "label"]),
-    #         Orientationd(keys=["image", "label"], axcodes="RAS"),
-    #         ThresholdIntensityd(keys=["label"], threshold=1, above=False, cval=1),
-    #         Spacingd(keys=["image", "label"], pixdim=(3, 3, 3), mode=("bilinear", "nearest")),
-    #         ResizeWithPadOrCropd(keys=["image", "label"], spatial_size=[96, 96, 96]),
-    #         ScaleIntensityRangePercentilesd(
-    #             keys=["image"],
-    #             lower=1.0,
-    #             upper=99.0,
-    #             b_min=-1.0,
-    #             b_max=1.0,
-    #             clip=True,
-    #             relative=False,
-    #         ),
-    #         ToTensord(keys=["image", "label"]),
-    #     ]
-    # )
     transforms = Compose(
         [
-            LoadImaged(keys=["image", "label"]),
-            AddChanneld(keys=["image", "label"]),
-            Orientationd(keys=["image", "label"], axcodes="RAS"),
-            ThresholdIntensityd(keys=["label"], threshold=1, above=False, cval=1),
-            Spacingd(keys=["image", "label"], pixdim=(3, 3, 3), mode=("bilinear", "nearest")),
-            ResizeWithPadOrCropd(keys=["image", "label"], spatial_size=[96, 96, 96]),
+            LoadImaged(keys=["T1", "T2"]),
+            AddChanneld(keys=["T1", "T2"]),
+            Orientationd(keys=["T1", "T2"], axcodes="RAS"),
+            ThresholdIntensityd(keys=["T2"], threshold=1, above=False, cval=1),
+            Spacingd(keys=["T1", "T2"], pixdim=(3, 3, 3), mode=("bilinear", "nearest")),
+            ResizeWithPadOrCropd(keys=["T1", "T2"], spatial_size=[96, 96, 96]),
             ScaleIntensityRangePercentilesd(
-                keys=["image"],
+                keys=["T1"],
                 lower=1.0,
                 upper=99.0,
                 b_min=-1.0,
@@ -138,7 +118,7 @@ if __name__ == "__main__":
                 clip=True,
                 relative=False,
             ),
-            ToTensord(keys=["image", "label"]),
+            ToTensord(keys=["T1", "T2"]),
         ]
     )
 
@@ -150,31 +130,31 @@ if __name__ == "__main__":
     hausdorff_distance = HausdorffDistanceMetric()
 
     # Loop to accessed images after transformations
-    for i in range(1): #range(len(test_images)):
+    for i in range(1): #range(len(test_T2s)):
         item = test_dataset.__getitem__(i)  # extract image and label from loaded dataset
         print(item)
         # save the t1w and label image
-        saver_t1w.save(data=item['image'], meta_data=item['image_meta_dict'])
-        saver_label.save(data=item['label'], meta_data=item['label_meta_dict'])
+        saver_t1w.save(data=item['T1'], meta_data=item['T1_meta_dict'])
+        saver_label.save(data=item['T2'], meta_data=item['T2_meta_dict'])
 
         # perform the inference
         with torch.no_grad():
             roi_size = (96, 96, 96)
             sw_batch_size = 1
             test_output = sliding_window_inference(
-                item['image'].unsqueeze(dim=0).to(device), roi_size, sw_batch_size, model
+                item['T1'].unsqueeze(dim=0).to(device), roi_size, sw_batch_size, model
             )
             out_im = torch.argmax(test_output, dim=1).detach().cpu() # convert from one hot encoding to 1 dimensional
 
         # create a metadata dictionary for the output by manipulating the input image meta data
-        out_meta_dict = item['image_meta_dict']
+        out_meta_dict = item['T1_meta_dict']
         out_meta_dict['filename_or_obj'] = out_meta_dict['filename_or_obj'].replace('T1w', 'predicted')
         saver_predicted.save(data=out_im, meta_data=out_meta_dict)  # save the predicted label to disk
 
         # # Prediction evaluation - metrics
         print(test_output) #TODO: analyze the intensity values of the one hot inferrence output
         # # create one hot encoding from the ground truth label
-        # one_hot_label = one_hot(item["label"].unsqueeze(dim=0), 2, dim=1)
+        # one_hot_label = one_hot(item["T2"].unsqueeze(dim=0), 2, dim=1)
 
         # # Run Mean Dice and Hausdorff Distance metrics using 2 different ways
         # mean_dice = compute_meandice(test_output.detach().cpu(), one_hot_label)
@@ -182,7 +162,7 @@ if __name__ == "__main__":
         # hausdorff = compute_hausdorff_distance(test_output.detach().cpu(), one_hot_label)
         # hausdorff2 = hausdorff_distance(test_output.detach().cpu(), one_hot_label)
         # # print the results
-        # print(item['image_meta_dict']['filename_or_obj'])
+        # print(item['T1_meta_dict']['filename_or_obj'])
         # print(f"Mean Dice: {mean_dice}")
         # print(f"Mean Dice: {mean_dice2}")
         # print(f"Hausdorff Distance: {hausdorff}")
