9258a759a70e493dcc9294317c3674988a57e6e0
Chase Johnson <cjohnson30@uiowa.edu>
2021-05-11 17:06:24 -0500

    Printing hte number of subjects for each data use.

--- code/metrics/count_data_breakdown.py
+++ code/metrics/count_data_breakdown.py
@@ -3,20 +3,19 @@ import json
 with open("../metadata/structure.json") as f:
     data = json.load(f)
 
-# print(data['train'])
-train_count = 0
-for sub in data['train']:
-    for sess in data['train'][sub]:
-        if len(data['train'][sub][sess]['t1w']) > 0 and len(data['train'][sub][sess]['t2w']) > 0:
-            train_count += 1
-
 def count_data_use(data_use):
+    sub_count = 0
     count = 0
     for sub in data[data_use]:
+        new_sub = False
         for sess in data[data_use][sub]:
             if len(data[data_use][sub][sess]['t1w']) > 0 and len(data[data_use][sub][sess]['t2w']) > 0:
                 count += 1
-    print(f"{data_use}: {count}")
+                new_sub = True
+        if new_sub:
+            sub_count += 1
+    print(f"{data_use} sessions: {count}")
+    print(f"{data_use} subjects: {sub_count}")
 
 count_data_use('train')
 count_data_use('test')
